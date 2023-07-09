35a106503365c0a68da64f18da2d71913111db4f
JotaPeans <jpfontesferreira1@gmail.com>
2023-07-09 14:08:45 -0300

    test

--- src/app/api/user/route.ts
+++ src/app/api/user/route.ts
@@ -3,7 +3,7 @@ import { NextResponse } from "next/server";
 
 const prisma = new PrismaClient();
 
-export async function GET(res: NextResponse) {
+export async function GET() {
     const users = await prisma.user.findMany();
     var newUsers = [];
     for(let user of users) {
